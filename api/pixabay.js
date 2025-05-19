c8908e11e5caf8def7ff42af5314f917e3a95361
suhanisingh04 <ssingh04@terpmail.umd.edu>
2025-05-19 06:30:10 -0400

    Update pixabay.js

--- api/pixabay.js
+++ api/pixabay.js
@@ -1,7 +1,7 @@
 // /api/pixabay.js
 
 export default async function handler(req, res) {
-    const API_KEY = process.env.PIXABAY_API_KEY; // ✅ Environment variable name
+    const API_KEY = process.env.PIXABAY_API_KEY; // Environment variable name
     const query = 'health+hospital+doctor+wellness';
   
     const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&per_page=10`;
