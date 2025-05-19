36f747dcefbcd57f183de7db94ac5b0f0a667a30
suhanisingh04 <ssingh04@terpmail.umd.edu>
2025-05-19 04:48:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         responsive: true,
         maintainAspectRatio: false,
         layout: {
-          padding: { left: 110, right: 30, top: 10, bottom: 10 }
+          padding: { left: 90, right: 30, top: 10, bottom: 10 }
         },
         plugins: {
           legend: {
